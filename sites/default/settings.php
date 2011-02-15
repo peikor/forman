6b8c7fc395ba4b5ec2fdd65848b5877d5e2f0dd5
alfonso <aheredia@mediaglobe.es>
2011-02-15 08:41:46 +0100

    version danland

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysqli://root:pc00@localhost/forman';
+$db_url = 'mysqli://forman:forman@localhost/forman';
 $db_prefix = '';
 
 /**
