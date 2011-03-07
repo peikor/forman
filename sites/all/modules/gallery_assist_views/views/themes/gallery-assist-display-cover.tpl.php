5427046fd23b52c225864787951cae5647b17163
peikor <peikor@yahoo.es>
2011-03-07 12:01:54 +0100

    ultima prueba
    Signed-off-by: peikor <peikor@yahoo.es>

---
+++ sites/all/modules/gallery_assist_views/views/themes/gallery-assist-display-cover.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: gallery-assist-display-cover.tpl.php,v 1.2 2010/09/09 04:52:13 jcmc Exp $
+
+/**
+ * @file
+ * @ingroup views_templates
+ */
+
+?>
+<div class="clear-block">
+  <?php print $gallery_assist_cover; ?>
+</div>
---
+++ sites/all/modules/gallery_assist_views/views/themes/gallery-assist-display-preview.tpl.php
@@ -0,0 +1,12 @@
+<?php
+// $Id: gallery-assist-display-preview.tpl.php,v 1.1 2010/09/09 04:52:13 jcmc Exp $
+
+/**
+ * @file
+ * @ingroup views_templates
+ */
+
+?>
+<div class="clear-block">
+  <?php print $preview_image; ?>
+</div>
